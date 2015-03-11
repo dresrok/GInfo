354822d27879f4f391c3e06bfcb67f6d30f16dd4
Andrés Felipe Santos <andresfesantos@gmail.com>
2015-03-10 21:55:37 -0500

    Wrapper guardar resultados
    Se genera un archivo de Excel con n hojas según los resultados para
    Comuna, Barrios, Corredores e Instituciones.

--- reportes.R
+++ reportes.R
@@ -57,3 +57,18 @@ densidadFollajeEspecifico <- function(comuna){
   );
   save.xlsx("F1_densidad_follaje.xlsx", follajeComuna, follajeBarrios, follajeCorredores, follajeInstituciones)
 }
+save.xlsx <- function (file, ...){
+  Sys.setenv(JAVA_HOME='C:\\Program Files\\Java\\jre8')
+  require(xlsx, quietly = TRUE)
+  objects <- list(...)
+  fargs <- as.list(match.call(expand.dots = TRUE))
+  objnames <- as.character(fargs)[-c(1, 2)]
+  nobjects <- length(objects)
+  for (i in 1:nobjects) {
+    if (i == 1)
+      write.xlsx(objects[[i]], file, sheetName = objnames[i])
+    else write.xlsx(objects[[i]], file, sheetName = objnames[i],
+                    append = TRUE)
+  }
+  print(paste("El archivo", file, "tiene", nobjects, "subhojas."))
+}
